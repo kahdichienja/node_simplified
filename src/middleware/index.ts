262e3db0f70abf516988de6259779cb584203088
kahdichinja <agooclinton@gmail.com>
2024-03-20 11:39:04 +0300

    abstracted req and res

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -2,6 +2,18 @@
 import 'reflect-metadata';
 import { Request, Response, NextFunction } from 'express';
 
+function getCurrentDateTimeWithYear(): string {
+  const now = new Date();
+  const year = now.getFullYear();
+  const month = String(now.getMonth() + 1).padStart(2, '0'); // Adding 1 because January is 0-indexed
+  const day = String(now.getDate()).padStart(2, '0');
+  const hours = String(now.getHours()).padStart(2, '0');
+  const minutes = String(now.getMinutes()).padStart(2, '0');
+  const seconds = String(now.getSeconds()).padStart(2, '0');
+
+  return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+}
+
 export function RegisterControllers(controllerClasses: any[]) {
   return function (target: any) {
     for (const controllerClass of controllerClasses) {
@@ -15,9 +27,11 @@ export function RegisterControllers(controllerClasses: any[]) {
         if (path && method) {
           target.app[method.toLowerCase()](path, (req: Request, res: Response, next: NextFunction) => {
             controllerInstance[methodName](req, res, next);
+            
+            console.info(`[${getCurrentDateTimeWithYear()} Method->${method}->${path}]`);
           });
+          console.info(`[Registered route: ${method} ${path}]`);
 
-          console.log(`[Registered route: ${method} ${path}]`);
         }
       }
     }
