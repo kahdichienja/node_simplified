8f4b7ed54277f41592246c182874e7a79a99d337
kahdichinja <agooclinton@gmail.com>
2024-03-20 09:57:05 +0300

    abstracted req and res

--- src/controllers/todoController.ts
+++ src/controllers/todoController.ts
@@ -1,4 +1,4 @@
-
+import { Request, Response } from 'express';
 import { Controller, Get } from '../core';
 
 @Controller() // Decorate UserController with @Controller() and specify base route path
@@ -6,6 +6,16 @@ export default class TodoController {
   @Get('/todos') // Decorate method with @Get() and specify route path
   getUsers() {
     // Controller logic to get users
+
     return ['Get all todos']; // Placeholder response
+  }
+
+  @Get('/users/:userId') // Decorate method with @Get() and specify route path with parameter
+  getUserById(req: Request, res: Response) {
+      const userId = req.params.userId; // Access request parameter
+      // Controller logic to get user by ID
+      return {
+        userId
+      };
   }
 }
